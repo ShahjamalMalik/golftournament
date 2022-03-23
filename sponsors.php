5556f1bad0e3b0cc70a0dc615b32acc0a3906bed
Linh_Nguyen <kaminguyen@hotmail.com>
2022-03-23 10:21:29 -0400

    Fix bug in sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -121,6 +121,10 @@
                             
                             echo '<h1></h1>';
                             echo '<div class="carousel-item active">';
+                            echo '<h4>Sponsor Name: "'.$pathsName[$i].'"</h4>';                    
+                            echo '<h4 "'.$hiddenDescription.'">Description: "'.trim($pathsDescription[$i], '"').'"</h4>';
+                            echo '<a  "'.$hiddenLink.'" href=""'.$pathsLink[$i].'""><h4>Website Link: "'.$pathsLink[$i].'"</h4></a>';
+
                             echo '<div class="form-group">';
                             echo '<form method="POST" action="server/deleteSponsorPicture.php" style="text-align:center" '.$hidden.'>';
                             echo '<button name="delete" value="'.$idArray[$i].'"> Delete Photo</button>';
